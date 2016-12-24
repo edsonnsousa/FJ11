0d73974859b142b441b35610a625382e831a702e
Edson Borges <edsonn.borgess@gmail.com>
2016-12-24 01:37:06 -0200

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,10 @@
+package FJ11;
+
+public  class Q2{
+	public static void main (String[] args) {
+	System.out.println("Analise");
+	System.out.println("e Desenvolvimento de Sistemas");
+	}
+	}
+
+
---
+++ Q3.java
@@ -0,0 +1,10 @@
+package FJ11;
+
+public  class Q3{
+	public static void main (String[] args) {
+	System.out.println("Analise \n e Desenvolvimento de Sistemas");
+	
+	}
+	}
+
+
